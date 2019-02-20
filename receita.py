1b1336846bbd5e4ef566a255442851ece167c8b5
Darlei Soares <darleisantossoares@gmail.com>
2019-02-20 16:34:59 -0300

    incremento do max_tries

--- receita.py
+++ receita.py
@@ -14,13 +14,18 @@ class Receita:
     def request(self, cnpj):
         tries = 0
         while True:
-            r = requests.post(self.URL_POST_RECEITA, data={'cnpj': cnpj})
+            with requests.Session() as session:
+                r = session.post(self.URL_POST_RECEITA, data={'cnpj': cnpj}, verify=False)
 
-            if r.status_code == requests.codes.ok:
-                return r.content
-                break
+                if r.status_code == requests.codes.ok:
+                    return r.text
+                    break
 
-            if tries > 2:
-                break
-                raise ValueError('Site da receita provavelmente fora do ar, tente novamente mais tarde')
-    
+                if tries > 2:
+                    break
+                    raise ValueError('Site da receita provavelmente fora do ar, tente novamente mais tarde')
+           
+            tries += 1 
+
+if __name__ == '__main__':
+    print(Receita().consulta("13449612000108"))
